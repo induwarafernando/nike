c1242d6053b96301c4821c362dc645357c542dc0
Induwara Fernando9 <induwarafernando9@gmail.com>
2023-10-30 00:16:43 +0530

    v11

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../components/bottom_nav_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: MyBottomNavBar(),
+    );
   }
 }
