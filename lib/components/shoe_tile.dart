4a0fb8fa16d50347099216ca0e3658022d22c46f
induwarafernando <induwarafernando9@gmail.com>
2023-11-08 22:53:33 +0530

    v14

--- lib/components/shoe_tile.dart
+++ lib/components/shoe_tile.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
 
+class ShoeTile extends StatelessWidget {
+  Shoe shoe;
+  ShoeTile({super.key, required this.shoe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 25),
+      width: 280,
+      decoration: BoxDecoration(
+        color: Colors.grey[100],
+        borderRadius: BorderRadius.circular(12),
+      ),
+    );
+  }
+}
